54ec83c94058f3a2399836a2f74d908d0a38275b
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-06-17 08:13:34 +0000

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-/**
- * main - Entry point 
-*/
 {
-/**
- * "puts" function to display the sentence, including the quotation marks at the beginning and end.
-*/
-    puts("\"Programming is like building a multilingual puzzle\"");
-/**
-* Return: Always 0 (Success)
-*/
-    return(0);
+puts("\"Programming is like building a multilingual puzzle\"");
+return (0);
 }
