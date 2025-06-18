4bef0f145ce27e31060724fd860d308760600a39
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-06-18 16:58:15 +0000

    6 Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * Print all the letters except q and e.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar((i % 10) + '0');
+}
+putchar('\n');
+
+return (0);
+
+}
