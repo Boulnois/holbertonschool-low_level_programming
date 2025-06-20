9fa6ab1784949b8046201671c3d6a1e32311c914
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-06-20 12:08:18 +0000

    1  alphabet game

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet
- * write the the lower-case alphabet on a line.
- */
+ * print_alphabet - Entry point
+ * write the the lower-case alphabet on a line
+ *
+ * Return: On success 1. 
+*/
 
 
 void print_alphabet(void)
@@ -11,7 +13,7 @@ void print_alphabet(void)
 {
 char letter;
 
-for (letter = "a"; letter <= "z"; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
 }
