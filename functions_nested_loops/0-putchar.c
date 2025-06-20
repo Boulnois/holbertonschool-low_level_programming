30a9b1f30eb575db85a1aacb7fbb3089c00cc88b
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-06-20 08:29:13 +0000

    0 putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints "main" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+_putchar('H');
+_putchar('e');
+_putchar('l');
+_putchar('l');
+_putchar('o');
+_putchar('\n');
+
+return (0);
+}
