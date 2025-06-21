ca9b0eae5a15f5f048b11df209e96f5365d53dc5
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-06-21 12:40:59 +0000

    10 a+b

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
-**
+/**
  * add - Write a function that adds two integers and returns the result.
  *
- * @a: int val
- * @b: int val
+ * @a: int val a
+ * @b: int val b
  *
  * Return: Sum of a + b
- */
+*/
 
 int add(int a, int b)
+
 {
 return (a + b);
 }
