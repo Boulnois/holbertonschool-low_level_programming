c8e54a5d296c4764e47c7a3aca201b4917e85aa0
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-06-23 09:14:04 +0000

    1 isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if the given character is an uppercase letter.
+ * @c: Checks for a digit (0 through 9)
+ *
+ * Return: 1 if c is a digit
+ */
+
+int _isdigit(int c)
+
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+return (0);
+}
