73fda03020df4d6dbfc65bc68ec2fadb53185e9c
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-06-24 21:54:07 +0000

    10 triangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,17 +1,27 @@
 #include "main.h"
 
 /**
- * print_triangle - print a triangle at a give size.
- * size of triangle to print.
- * If size is 0 or -, the function print only a new line.
- * 1. Print the spaces
- * 2. Print the #
- * 3. Print a new line after each line of the triangle
- * Return: Always 0.
- */
+    * print_triangle - Prints a triangle of given size
+    * @size: The size of the triangle
+    *
+    * If size is 0 or negative, print only a new line.
+    *
+    * This function performs the following:
+    * 1. Prints the spaces necessary for triangle formatting
+    * 2. Prints the '#' symbols to form the triangle
+    * 3. Prints a new line after each line of the triangle
+    */
 
 void print_triangle(int size)
 {
+/*
+ * int i: for iteration in for loop also use as a way to subtract.
+ *
+ * int margin: will recored the amount of whitesapce ill need to print.
+ *
+ * int toPrint: is used to tell how many '#' will need to be print per-line
+ */
+
 	int i, margin, toPrint;
 
 	if (size <= 0)
