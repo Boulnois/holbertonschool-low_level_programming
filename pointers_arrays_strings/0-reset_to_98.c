7b6fee61f8e061757e2fab42cc3e9290709babe8
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-06-25 09:40:42 +0000

    0 Battery st

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 /**
- * takes a pointer to an int
- *
+ * reset_to_98 - updates the value it points
+ * @n: pointer
  * Return: Always 0.
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
