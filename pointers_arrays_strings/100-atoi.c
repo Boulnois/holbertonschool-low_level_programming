9d0eafae04f023010e665482fb7a1eb02a78e0b5
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-06-29 11:25:23 +0000

    10. Great leaders

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,41 @@
-#include "main.h"
-#include <stdio.h>
+Ú#include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _atoi - convert a string to an integer
+ * @s: number in the string can be preceded by an infinite number of characters
+ * You need to take into account all the - and + signs before the number
+ * Return: number and sign
  */
 
 int _atoi(char *s)
 {
-	int scan = 0;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+	int numFound = 0;
 
-	while (s[scan] != '\0')
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9') && s[i] != '-' && s[i] != '+')
 	{
-		if (*s >= 'A' && *s <= 'Z')
-			{printf("%d", *s);}
-		else if (*s == '+' || *s == '-')
-			{printf("%d", *s);}
-		else
-		{printf("%d", *s);}
-	printf("scan = %d\n", *s[scan]);
-
-	chiffre - '0'
-	*s++;
-	return;
+		i++;
 	}
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			numFound = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else
+		{
+			break;
+		}
+		i++;
+	}
+
+	return (numFound ? result * sign : 0);
 }
