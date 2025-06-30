8ecc7f3b60bea962f7e6a3c796b6b56857ccf371
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-06-30 22:11:25 +0000

    5. strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,32 +1,46 @@
 #include "main.h"
 #include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 
 /**
  * _strstr - Function that locates a substring
  * @needle: char pointer
  * @haystack: char pointer
- * Return:  a pointer to the beginning of the located substring
+ * Return: a pointer to the beginning of the located substring
  */
 
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int count;
+	unsigned int haystack_len = strlen(haystack);
+	unsigned int needle_len = strlen(needle);
 
 	if (haystack == NULL || needle == NULL)
 	{
 		return (NULL);
 	}
 
+	if (needle_len == 0)
+	{
+		return (haystack);
+	}
+
 	for (; haystack[0]; haystack++)
 	{
+		if (haystack_len < needle_len)
+		{
+			return (NULL);
+}
 		for (count = 0; haystack[count] == needle[count]; count++)
 		{
-			if (!(needle[count]))
-			return (haystack);
+			if (count == needle_len - 1)
+			{
+				return (haystack);
+			}
 		}
 
 	}
-	return (0);
+	return (NULL);
 }
 
