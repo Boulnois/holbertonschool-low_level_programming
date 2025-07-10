1be2d64c0db29d9ed315e6bf350addab2c51e589
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-07-10 20:32:06 +0000

    2. print

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -22,7 +22,14 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: (nil)\n");
 	}
-	printf("Age: %.6f\n", d->age);
+	if (d->age < 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %.6f\n", d->age);
+	}
 	if (d->owner != NULL)
 	{
 		printf("Owner %s\n", d->owner);
