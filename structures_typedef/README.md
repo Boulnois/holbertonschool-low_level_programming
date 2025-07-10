95bb620d47b259f35508eb095d3b27e7bc0b32e3
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-07-10 11:00:36 +0000

    is the type char*

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+Exercice on C - Structures, typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new type struct dog with elements
+ *@name: name is the type char*
+ *@age: age is the type float
+ *@owner: owner is the type char*
+ * Return:  new type struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
