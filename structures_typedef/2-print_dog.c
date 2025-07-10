4879fa712ac2639b02707de86efbac7306383a49
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-07-10 15:16:49 +0000

    2. C - Structures, typedef

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_dog(struct dog *d
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
