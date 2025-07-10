305b7ead9466d3157ec170478c42a87cd5cbf855
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-07-10 11:37:51 +0000

    3. Outside of a do

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,17 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * struct dog - new type struct dog with elements
- *@name: name is the type char*
- *@age: age is the type float
- *@owner: owner is the type char*
- * Return:  new type struct dog
+ * typedef struct dog - new type dog_t struct dog with elements
+ * @name: name is the type char*
+ * @age: age is the type float
+ * @owner: owner is the type char*
+ * @dog_t: a new type
+ * Return: a new type dog_t as a new name for the type struct dog
  */
-struct dog
+
+typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-};
+	char *name; /** Name of the dog */
+	float age; /** age of the dog */
+	char *owner; /** owner of the dog */
+} dog_t;
 
 #endif
