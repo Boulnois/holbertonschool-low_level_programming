f38750b5e244e7d66b82327f733c999785e310e7
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-07-28 14:14:45 +0000

    0. Print list

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -11,12 +11,12 @@
 
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
-int _strlen(char *s);
+int _str≈Ò≈©len(char *s);
 
 #endif
