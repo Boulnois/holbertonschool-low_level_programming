6e8f21ba903cd0d8c5a1ffa3f3633a98ef4939ac
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-07-28 13:39:20 +0000

    0. Print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,9 +4,13 @@
 #include "lists.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_list - Prints all the elements of a list_t list
+ * @h: Pointer to the head of the linked list.
+ * Description:
+ * For each node, prints:
+ * - [len] str     if str is not NULL
+ * - [0] (nil)     if str is NULL
+ * Return: The number of nodes in the list.
  */
 
 size_t print_list(const list_t *h)
@@ -19,12 +23,12 @@ size_t print_list(const list_t *h)
 		{
 			printf("[%u] %s\n",h->len, h->str);
 		}
-        else
-        {
-                printf("[0] (nil)\n");
-        }
-	    size++;
-	    h = h->next;
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+	size++;
+	h = h->next;
 	}
 	return size;
 }
---
+++ singly_linked_lists/main.h~
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Calcule la longueur d'une chaîne de caractères
+ * @s: La chaîne dont la longueur doit être mesurée
+ *
+ * Return: La longueur de la chaîne
+*/
+
+int _strlen(char *s)
+{
+	int chaine = 0;
+
+	while (*(s + chaine) != '\0')
+	{
+	chaine++;
+	}
+
+	return (chaine);
+}
