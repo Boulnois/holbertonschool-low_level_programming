fa050150c6ed1f1677cd7384bcd2350962e75877
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-07-29 14:20:06 +0000

    2. Add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,23 +9,28 @@
  *
  * Return: Address of the new node, or NULL if it failed
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+    list_t *new_node;
 
-	if (new_node == NULL)
-		return (NULL);
+    if (!head || !str)
+        return NULL;
 
-	new_node->str = strdup((char *)str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+    new_node = malloc(sizeof(list_t));
+    if (!new_node)
+        return NULL;
 
-	new_node->len = strlen(new_node->str);
-	new_node->next = *head;
-	*head = new_node;
+    new_node->str = strdup(str);
+    if (!new_node->str)
+    {
+        free(new_node);
+        return NULL;
+    }
 
-	return (new_node);
+    new_node->len = strlen(new_node->str);
+    new_node->next = *head;
+    *head = new_node;
+
+    return new_node;
 }
