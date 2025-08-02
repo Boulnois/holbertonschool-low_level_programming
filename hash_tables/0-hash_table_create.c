29a2ff94c1b6f2a75e3f78e9750f4a8f15a75b95
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-08-02 15:00:18 +0000

    0.creates a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -37,25 +37,27 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *node, *tmp;
-    unsigned long int i;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-    if (!ht)
-        return;
+	if (!ht)
+	{
+		return;
+	}
 
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        while (node)
-        {
-            tmp = node;
-            node = node->next;
-            free(tmp->key);
-            free(tmp->value);
-            free(tmp);
-        }
-    }
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
 
-    free(ht->array);
-    free(ht);
+	free(ht->array);
+	free(ht);
 }
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -32,5 +32,6 @@ typedef struct hash_table_s
 
 
 hash_table_t *hash_table_create(unsigned long int size);
+void hash_table_delete(hash_table_t *ht);
 
 #endif
