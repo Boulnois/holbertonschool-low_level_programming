591af8d146df4409dd67779aa6d09f7b9e133b3e
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-08-02 14:50:42 +0000

    0.creates a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -34,3 +34,28 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	return (ht);
 }
+
+void hash_table_delete(hash_table_t *ht)
+{
+    hash_node_t *node, *tmp;
+    unsigned long int i;
+
+    if (!ht)
+        return;
+
+    for (i = 0; i < ht->size; i++)
+    {
+        node = ht->array[i];
+        while (node)
+        {
+            tmp = node;
+            node = node->next;
+            free(tmp->key);
+            free(tmp->value);
+            free(tmp);
+        }
+    }
+
+    free(ht->array);
+    free(ht);
+}
