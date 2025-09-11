7d87422b8ffb93bba86c19dec32c894eeab0e5a4
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-09-11 17:02:32 +0200

    Add print_times_table function using _putchar

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -9,8 +9,8 @@ void print_number(int n)
 	if (n >= 100)
 	{
 		_putchar(n / 100 + '0');
-			_putchar('0' + (n / 10) % 10);
-			_putchar(n % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 	else if (n >= 10)
 	{
