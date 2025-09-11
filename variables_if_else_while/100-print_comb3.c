83a0f949f9a4dc264662db946687ee19601855d8
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-09-11 12:52:29 +0200

    Add program 100-print_comb3.c: prints all combinations of two digits

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - affiche toutes les combinaisons possibles de deux chiffres
- *         en ordre croissant, séparées par une virgule et un espace.
- *         Les chiffres doivent être différents et chaque combinaison
- *         ne doit apparaître qu'une seule fois.
- *
- * Return: 0
- */
+* main - affiche toutes les combinaisons possibles de deux chiffres
+*         en ordre croissant, séparées par une virgule et un espace.
+*         Les chiffres doivent être différents et chaque combinaison
+*         ne doit apparaître qu'une seule fois.
+*
+* Return: 0
+*/
 int main(void)
 {
 	int nbr_1, nbr_2;
 
-	for (nbr_1 = 0; nbr_1 <=8; nbr_1++)
-	{
-		for (nbr_2 = nbr_1 + 1; nbr_2 <= 9; nbr_2++) 
+		for (nbr_1 = 0; 8 >= nbr_1; nbr_1++) 
 		{
-			putchar(nbr_1 + '0');
-			putchar(nbr_2 + '0');
-			
-			if (nbr_1 != 8 || nbr_2 != 9)
+			for (nbr_2 = nbr_1 + 1; nbr_2 <= 9; nbr_2++) 
 			{
+				putchar(nbr_1 + '0');
+				putchar(nbr_2 + '0');
+
+				if (nbr_1 != 8 || nbr_2 != 9) 
+				{
 				putchar(',');
 				putchar(' ');
+				}
 			}
 		}
-	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
