37b902e63f27278e3bff69ae7df3eaf109adf3cf
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-09-12 16:42:49 +0200

    Add 100-prime_factor.c with Betty-compliant comments

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - finds and prints the largest prime factor of 612852475143
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+	long n = 612852475143;
+	long largest_factor = -1;
+	long i;
+	long limit = sqrt(n);
+
+	while (n % 2 == 0)
+	{
+		largest_factor = 2;
+		n = n / 2;
+	}
+
+	for (i = 3; i <= limit; i += 2)
+	{
+		while (n % i == 0)
+		{
+			largest_factor = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+	{
+		largest_factor = n;
+	}
+
+	printf("%ld\n", largest_factor);
+	return (0);
+}
