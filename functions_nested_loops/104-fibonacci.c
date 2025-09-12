c89ea4a8b2a11ea914901a47de8d0ca2b0ce9e95
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-09-12 12:21:46 +0200

    Add 104-fibonacci.c: prints first 98 Fibonacci numbers

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -8,19 +8,36 @@
 */
 int main(void)
 {
-int nbr_1 = 1, nbr_2 = 2, next, i;
+	int i;
+
+	unsigned long nbr_1_low = 1, nbr_2_low = 2;
+	unsigned long nbr_1_high = 0, nbr_2_high = 0;
+	unsigned long next_low, next_high;
+	unsigned long billion = 1000000000;
+
+	printf("%lu, %lu", nbr_1_low, nbr_2_low);
+
 
 for (i = 1; i <= 98; i++)
 	{
-		printf("%d", nbr_1);
+		next_low = nbr_1_low + nbr_2_low;
+		next_high = nbr_1_high + nbr_2_high;
 
-		if (i != 98)
+		if (next_low >= billion)
 		{
-			printf(", ");
+			next_low -= billion;
+			next_high += 1;
 		}
-		next = nbr_1 + nbr_2;
-		nbr_1 = nbr_2;
-		nbr_2 = next;
+
+		if (next_high > 0)
+			printf(", %lu%09lu", next_high, next_low);
+		else
+			printf(", %lu", next_low);
+
+		nbr_1_low = nbr_2_low;
+		nbr_1_high = nbr_2_high;
+		nbr_2_low = next_low;
+		nbr_2_high = next_high;
 	}
 	printf("\n");
 	return (0);
