1abb0654792b96e3cb71d966bca0f984e809737a
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-09-15 14:25:50 +0200

    a function that prints a string

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,70 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
-* valid - character set used to generate the password
-*/
-static const char valid[] =
-
-	"abcdefghijklmnopqrstuvwxyz"
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	"0123456789"
-	"!@#$%^&*()-_=+[]{};:,.<>?/";
-
-/**
-* VALID_LEN - length of the valid character set
-*/
-#define VALID_LEN (sizeof(valid) - 1)
-
-/**
-* PASS_LEN - length of the password to generate
-*/
-#define PASS_LEN 16
-
-/**
-* rand_char_from_valid - picks a random character from the valid set
+* main - generates a random password for 101-crackme
 *
-* Return: a random character
-*/
-char rand_char_from_valid(void)
-{
-	return (valid[rand() % VALID_LEN]);
-}
-
-/**
-* gen_password - generates a random password
-* @buf: buffer to store the password
-* @len: length of the password (excluding null terminator)
-*
-* Return: nothing
-*/
-void gen_password(char *buf, size_t len)
-{
-	size_t i;
-
-	for (i = 0; i < len; i++)
-	{
-		buf[i] = rand_char_from_valid();
-	}
-	buf[len] = '\0';
-}
-
-/**
-* main - entry point
-*
-* Return: always 0
+* Return: Always 0
 */
 int main(void)
 {
-	char pass[PASS_LEN + 1];
+	char pass[16 + 1];
 	size_t i;
+	unsigned int seed;
 
-	srand((unsigned int)time(NULL));
-	gen_password(pass, PASS_LEN);
+	/* initialize random seed with current time */
+	seed = (unsigned int)time(NULL);
 
-	for (i = 0; i < PASS_LEN; i++)
+	srand(seed);
+
+	/* generate 16 random printable ASCII characters */
+	for (i = 0; i < 16; i++)
+	{
+		pass[i] = 33 + (rand() % 94); /* ASCII from '!' to '~' */
+	}
+	pass[16] = '\0';
+
+	/* print password using _putchar */
+	for (i = 0; i < 16; i++)
 	{
 		_putchar(pass[i]);
 	}
