65694c39eb051ee8ccd34dca45ff80702ba24deb
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-09-15 13:44:49 +0200

    a function that prints a string

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,67 +1,73 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
- * valid - character set used to generate the password
- */
+* valid - character set used to generate the password
+*/
 static const char valid[] =
-    "abcdefghijklmnopqrstuvwxyz"
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    "0123456789"
-    "!@#$%^&*()-_=+[]{};:,.<>?/";
+
+	"abcdefghijklmnopqrstuvwxyz"
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	"0123456789"
+	"!@#$%^&*()-_=+[]{};:,.<>?/";
 
 /**
- * VALID_LEN - length of the valid character set
- */
+* VALID_LEN - length of the valid character set
+*/
 #define VALID_LEN (sizeof(valid) - 1)
 
 /**
- * PASS_LEN - length of the password to generate
- */
+* PASS_LEN - length of the password to generate
+*/
 #define PASS_LEN 16
 
 /**
- * rand_char_from_valid - picks a random character from the valid set
- *
- * Return: a random character
- */
+* rand_char_from_valid - picks a random character from the valid set
+*
+* Return: a random character
+*/
 char rand_char_from_valid(void)
 {
-    return (valid[rand() % VALID_LEN]);
+	return (valid[rand() % VALID_LEN]);
 }
 
 /**
- * gen_password - generates a random password
- * @buf: buffer to store the password
- * @len: length of the password (excluding null terminator)
- *
- * Return: nothing
- */
+* gen_password - generates a random password
+* @buf: buffer to store the password
+* @len: length of the password (excluding null terminator)
+*
+* Return: nothing
+*/
 void gen_password(char *buf, size_t len)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < len; i++)
-        buf[i] = rand_char_from_valid();
-    buf[len] = '\0';
+	for (i = 0; i < len; i++)
+		buf[i] = rand_char_from_valid();
+	buf[len] = '\0';
 }
 
 /**
- * main - entry point
- *
- * Return: always 0
- */
+* main - entry point
+*
+* Return: always 0
+*/
 int main(void)
 {
-    char pass[PASS_LEN + 1];
-    time_t seed = time(NULL);
+	char pass[PASS_LEN + 1];
 
-    srand((unsigned int) seed);
-    printf("seed = %ld\n", (long) seed);
+	size_t i;
 
-    gen_password(pass, PASS_LEN);
-    printf("password: %s\n", pass);
+	srand((unsigned int)time(NULL));
 
-    return (0);
+	gen_password(pass, PASS_LEN);
+
+	/* Print password using _putchar */
+	for (i = 0; i < PASS_LEN; i++)
+		_putchar(pass[i]);
+	_putchar('\n');
+
+	return (0);
 }
