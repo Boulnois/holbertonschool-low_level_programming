44fba5f9119d8976d831b0d298ee37d0ab5ba823
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-09-16 11:50:14 +0200

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,31 +1,30 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: Creates an array, modifies its third element using a
- * pointer, and prints the value of that element using _putchar.
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: Creates an array, modifies its third element using a
+* pointer, and prints the value of that element using _putchar.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int a[5];
-	int *pt;
+	int *p;
 
 	a[2] = 1234;
-	pt = &a[2];
+	p = &a[2];
 
-	*pt = 98;
+	*p = 98;
 	_putchar('a');
-	_putchar('[');	
+	_putchar('[');
 	_putchar('2');
 	_putchar(']');
 	_putchar(' ');
 	_putchar('=');
-	_putchar((*pt / 10) + '0');	
-	_putchar((*pt % 10) + '0');	
+	_putchar((*p / 10) + '0');
+	_putchar((*p % 10) + '0');
 	_putchar('\n');
 
 return (0);
