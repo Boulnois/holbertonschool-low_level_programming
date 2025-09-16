c7a20c2c81ac3aa34a9e1cecb9dda1aa9b8aaab0
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-09-16 12:45:32 +0200

    Add 102-magic.c: prints a[2] = 98 using _putchar

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -4,17 +4,17 @@
 * main - Entry point
 * Description: Creates an array and prints the value of the third element
 *              using a pointer.
+*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
 	int a[5];
+
 	int *p;
 
 	a[2] = 1024;
 	p = &a[2];
-
-	*p = 98;
 
 	_putchar('a'); _putchar('['); _putchar('2'); _putchar(']'); _putchar(' '); _putchar('='); _putchar(' '); _putchar((*p / 10) + '0'); _putchar((*p % 10) + '0'); _putchar('\n');
 
