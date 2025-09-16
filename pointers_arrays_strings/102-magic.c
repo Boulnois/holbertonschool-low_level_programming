98d96fdc122b24101d336cc02c63f8033a6f7496
‘Boulnois’ <georgia.boulnois@gmail.com>
2025-09-16 13:07:46 +0200

    Add 102-magic.c: prints a[2] = 98 using _putchar

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -10,11 +10,11 @@
 int main(void)
 {
 	int a[5];
-
 	int *p;
 
 	a[2] = 1024;
 	p = &a[2];
+	*p = 98;
 
 	_putchar('a'); _putchar('['); _putchar('2'); _putchar(']'); _putchar(' '); _putchar('='); _putchar(' '); _putchar((*p / 10) + '0'); _putchar((*p % 10) + '0'); _putchar('\n');
 
